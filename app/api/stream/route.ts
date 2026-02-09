6c783ac270fa5fe99b07b70a25b862fbb68dce4e
nancy-kataria <katarianancy8@gmail.com>
2026-02-08 18:22:12 -0800

    text streaming

---
+++ app/api/stream/route.ts
@@ -0,0 +1,18 @@
+import { streamText } from "ai";
+import { openai } from "@ai-sdk/openai";
+
+export async function POST(req: Request){
+    try {
+    const { prompt } = await req.json();
+
+    const result = await streamText({
+      model: openai("gpt-4.1-nano"),
+      prompt,
+    });
+
+    return result.toUIMessageStreamResponse();
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Failed to stream text" }, { status: 500 });
+  }
+}
