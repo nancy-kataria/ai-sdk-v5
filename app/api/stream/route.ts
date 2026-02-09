bb9581d113c523c72fa1d86040a819bba86a39c7
nancy-kataria <katarianancy8@gmail.com>
2026-02-08 18:37:01 -0800

    tracing token usage

--- app/api/stream/route.ts
+++ app/api/stream/route.ts
@@ -10,6 +10,15 @@ export async function POST(req: Request){
       prompt,
     });
 
+    // tracing token usage
+    result.usage.then((usage) => {
+        console.log({
+            inputTokens: usage.inputTokens,
+            outputTokens: usage.outputTokens,
+            totalTokens: usage.totalTokens
+        })
+    })
+
     return result.toUIMessageStreamResponse();
   } catch (error) {
     console.error(error);
